36aa0f439da8e56e397a14a2bf715a1870a0d499
David DeSandro <desandrocodes@gmail.com>
2016-11-15 12:12:33 -0500

    standing  horse, tall elephant sold

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp');
 var site = {
   // templating data
   data: {
-    currentPrice: 680,
+    currentPrice: 720,
     isDev: process.argv[2] == 'dev',
   },
   templates: {},
